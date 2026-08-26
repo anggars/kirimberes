c8d6e2e31888eda266f429a3fac572b6cc355a03
anggars <anggariantosudrajat@gmail.com>
2026-08-26 16:56:51 +0700

    up

--- src/components/users-client.tsx
+++ src/components/users-client.tsx
@@ -79,11 +79,10 @@ export function UsersClient({ initialUsers }: { initialUsers: Omit<User, 'passwo
           setIsOpen(open)
           if (!open) resetForm()
         }}>
-          <DialogTrigger asChild>
-            <Button className="shadow-md">
+          <DialogTrigger render={<Button className="shadow-md">
               <PlusCircle className="mr-2 h-4 w-4" />
               Tambah User
-            </Button>
+            </Button>}>
           </DialogTrigger>
           <DialogContent>
             <DialogHeader>
